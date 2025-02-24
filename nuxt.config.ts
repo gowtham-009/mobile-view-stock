151190390fca15edad141c4a80bbf06e7da65d81
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-02-24 11:22:51 +0530

    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,15 @@ import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
     devtools: { enabled: false },
+
+    app: {
+        head: {
+          title: 'Stock Table',
+          meta: [
+            { name: 'description', content: 'This is my awesome Nuxt 3 application' }
+          ],
+        }
+      },
     css: ['@/assets/css/global.css'],
     modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
     primevue: {
