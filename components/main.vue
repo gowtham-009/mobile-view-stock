8f90bcd7116e092c1b80fb180b38870444aa53e9
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-02-24 10:39:09 +0530

    changenewone

--- components/main.vue
+++ components/main.vue
@@ -201,22 +201,22 @@
     <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2  bg-white px-4 py-10 sm:px-6 xl:px-8">
       <dt class="text-sm/6 font-medium text-gray-500">Invested Amount</dt>
       <dd class="text-xs font-medium text-gray-700">+4.75%</dd>
-      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight  text-gray-900">₹5000</dd>
+      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight  text-gray-900">₹{{ investamount }}</dd>
     </div>
     <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
       <dt class="text-sm/6 font-medium text-gray-500">Current Value</dt>
       <dd class="text-xs font-medium text-rose-600">+54.02%</dd>
-      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight text-gray-900">₹6000</dd>
+      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight text-gray-900">₹{{ currentvalue }}</dd>
     </div>
     <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
       <dt class="text-sm/6 font-medium text-gray-500">Overall Gain</dt>
       <dd class="text-xs font-medium text-gray-700">-1.39%</dd>
-      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight text-gray-900">₹7000</dd>
+      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight text-gray-900">₹{{ overallgain }}</dd>
     </div>
     <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
       <dt class="text-sm/6 font-medium text-gray-500">Today's Gain</dt>
       <dd class="text-xs font-medium text-rose-600">+10.18%</dd>
-      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight text-gray-900">₹8000</dd>
+      <dd class="w-full flex-none text-3xl/10 font-medium tracking-tight text-gray-900">₹{{ todaysgain }}</dd>
     </div>
   </dl> 
       </div>
@@ -630,9 +630,39 @@ const customtoggle = () => {
     return date.toLocaleDateString('en-GB').split('/').join('-'); // Format to dd-mm-yyyy
     };
 
+    const investamount=ref('')
+    const currentvalue=ref('')
+    const overallgain=ref('')
+    const todaysgain=ref('')
 
     const getFilteredData = (dateFilterValue) => {
      
+      if(dateFilterValue==='days_7'){
+        investamount.value='50000'
+        currentvalue.value='20000'
+        overallgain.value='30000'
+        todaysgain.value='45000'
+      }
+      else if(dateFilterValue==='days_15'){
+        investamount.value='25000'
+        currentvalue.value='26000'
+        overallgain.value='28000'
+        todaysgain.value='98000'
+      }
+      else if(dateFilterValue==='month_1'){
+        investamount.value='2000'
+        currentvalue.value='27000'
+        overallgain.value='38000'
+        todaysgain.value='55000'
+      }
+
+      else if(dateFilterValue==='months_3'){
+        investamount.value='6300'
+        currentvalue.value='42000'
+        overallgain.value='98000'
+        todaysgain.value='78000'
+      }
+
       visibleTop.value = false
     const today = new Date();
     today.setHours(23, 59, 59, 999);
